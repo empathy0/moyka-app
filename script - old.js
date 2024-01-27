a04ec5ab7e8cd6125c2ab12d63b29166d2a361d5
empathy0 <mquliev79@gmail.com>
2024-01-27 22:52:33 +0400

    search functionality alpha -fix highlight -fix description when search

--- script - old.js
+++ script - old.js
@@ -19,6 +19,7 @@ function updateMap(filteredData) {
   });
 }
 
+
 function performSearch() {
   // Get the search query from the input element
   const searchQuery = document.getElementById('searchInput').value.toLowerCase();
@@ -38,33 +39,9 @@ function performSearch() {
   }
 }
 
-function processDataAndMap() {
-  // Add GeoJSON data as a source
-  map.addSource('some-points', { type: 'geojson', data: data });
-
-  // Add markers for each point feature
-  data.features.forEach(function(feature) {
-      var coordinates = feature.geometry.coordinates;
-      var popupContent = `
-          <strong>${feature.properties.name}</strong><br>
-          Rating: ${feature.properties.rating}<br>
-          Address: ${feature.properties.address}<br>
-          <!-- <a href="${feature.properties.link}" target="_blank">Link</a> -->
-          <button onclick="openInWaze(${coordinates[1]}, ${coordinates[0]})">Waze-də aç</button>
-          <button onclick="openInGoogleMaps(${coordinates[1]}, ${coordinates[0]})">Google Maps-da aç</button>
-      `;          
-      var markerId = feature.id;
-
-      // Create a new marker and add it to the map
-      new mapboxgl.Marker()
-          .setLngLat(coordinates)
-          .setPopup(new mapboxgl.Popup().setHTML(popupContent))
-          .addTo(map);
-  });
-}
 
 (function() {
-  map = new mapboxgl.Map({
+  var map = new mapboxgl.Map({
       container: 'map',
       style: 'mapbox://styles/mapbox/streets-v9',
       center: [49.8302375129846, 40.39577415685276],
@@ -88,12 +65,29 @@ function processDataAndMap() {
     // Fetch GeoJSON data from file
     fetch('https://raw.githubusercontent.com/empathy0/moyka-app/0ad3c8c498804d15f74a8aeb156aa14de48214d3/data_geojson.json')
     .then(response => response.json())
-    .then(fetchedData => {
-        // Assign the fetched data to the global variable
-        data = fetchedData;
+    .then(data => {
+        // Add GeoJSON data as a source
+        map.addSource('some-points', { type: 'geojson', data: data });
 
-        // Process the data and update the map
-        processDataAndMap();
+        // Add markers for each point feature
+        data.features.forEach(function(feature) {
+            var coordinates = feature.geometry.coordinates;
+            var popupContent = `
+                <strong>${feature.properties.name}</strong><br>
+                Rating: ${feature.properties.rating}<br>
+                Address: ${feature.properties.address}<br>
+                <!-- <a href="${feature.properties.link}" target="_blank">Link</a> -->
+                <button onclick="openInWaze(${coordinates[1]}, ${coordinates[0]})">Waze-də aç</button>
+                <button onclick="openInGoogleMaps(${coordinates[1]}, ${coordinates[0]})">Google Maps-da aç</button>
+            `;          
+            var markerId = feature.id;
+
+            // Create a new marker and add it to the map
+            new mapboxgl.Marker()
+                .setLngLat(coordinates)
+                .setPopup(new mapboxgl.Popup().setHTML(popupContent))
+                .addTo(map);
+        });
     })
     .catch(error => console.error('Error fetching GeoJSON:', error));       
   });
